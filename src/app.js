7e94d382ec46e716d024285fcff6bc733ce7d48c
VladimirSobolev1978 <sobolev.exceedteam@gmail.com>
2021-07-20 17:20:08 +0300

    fixed heroku build

--- src/app.js
+++ src/app.js
@@ -10,12 +10,12 @@ const errorHandler = require('./middlewares/errorHandler');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '../client', 'build')));
+
 
 app.use('api/tasks', taskRouter);
 app.use('api/auth', authRouter);
 mongoose.connect(process.env.MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true });
-
+app.use(express.static(path.join(__dirname, '../client', 'build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
 });
